5fc2f570c514128a413d03bf460952a4186437c7
xq <1254807175@qq.com>
2025-03-12 23:51:30 +0800

    忽略，修改

--- src/main/java/org/review/Main.java
+++ src/main/java/org/review/Main.java
@@ -1,12 +1,13 @@
 package org.review;
 
-import org.review._25_03.Code_25_03_05_209;
+
+import org.review._25_03._01_10.Code_05_209;
 
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
-        Code_25_03_05_209 solution = new Code_25_03_05_209();
+        Code_05_209 solution = new Code_05_209();
         int[] inputs = new int[]{2,3,1,2,4,3};
         int target = 7;
 //        int[] inputs = new int[]{1,4,4};
