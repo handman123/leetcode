69c066996525dba07832b60f4c7fc7a25a704c46
xq <1254807175@qq.com>
2025-03-14 23:42:46 +0800

    128. 最长连续序列（未完成）

---
+++ src/main/java/org/whut/code/hash/Code_03.java
@@ -0,0 +1,24 @@
+package org.whut.code.hash;
+
+/**
+ * <p>
+ *     128. 最长连续序列
+ * </p>
+ * <p>
+ *     给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * </p>
+ * <p>
+ *     请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * </p>
+ */
+public class Code_03 {
+    /**
+     * 1. 用map存储数字
+     *
+     * @param nums
+     * @return
+     */
+    public int longestConsecutive(int[] nums) {
+
+    }
+}
