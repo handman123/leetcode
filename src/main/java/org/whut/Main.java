4dd07385b767028db3f05371ea49cfc74939922c
xq <1254807175@qq.com>
2025-03-14 23:24:28 +0800

    49. 字母异位词分组

--- src/main/java/org/whut/Main.java
+++ src/main/java/org/whut/Main.java
@@ -1,13 +1,13 @@
 package org.whut;
 
-import org.whut.code.Code_hash_01;
+import org.whut.code.hash.Code_01;
 
 import java.util.Arrays;
 
 public class Main {
 
         public static void main(String[] args) {
-            Code_hash_01 solution = new Code_hash_01();
+            Code_01 solution = new Code_01();
 
             int[] input = new int[]{};
             int target = 0;
