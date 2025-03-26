56ea8c934c48c5a3c3ea873516fbcd5c920e7729
xq <1254807175@qq.com>
2025-03-26 16:56:41 +0800

    链表：141. 环形链表

---
+++ src/main/java/org/whut/code/link/Code04.java
@@ -0,0 +1,45 @@
+package org.whut.code.link;
+
+
+/**
+ * <p>
+ *     141. 环形链表
+ * </p>
+ * <p>
+ *     给你一个链表的头节点 head ，判断链表中是否有环。
+ * </p>
+ * <p>
+ *     如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * </p>
+ */
+public class Code04 {
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode vHead = new ListNode(-1);
+        vHead.next = head;
+
+        ListNode fast = vHead, slow = vHead;
+
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow){
+                break;
+            }
+        }
+
+        return fast != null && fast.next != null;
+
+//        fast = vHead;
+//        while (fast != slow){
+//            fast = fast.next;
+//            slow = slow.next;
+//        }
+
+    }
+}
