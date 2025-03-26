f3d0f0074c24d81b85b7a83baf7003f1abf98d32
xq <1254807175@qq.com>
2025-03-26 16:28:35 +0800

    链表：node

---
+++ src/main/java/org/whut/code/link/ListNode.java
@@ -0,0 +1,10 @@
+package org.whut.code.link;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
